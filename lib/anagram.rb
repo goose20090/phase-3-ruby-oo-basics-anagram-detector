d590dba7de669fea15c4e131f1e356c367bd5929
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-18 10:15:19 +0000

    commit tree a4f4838b883b9fb4593eea014b5e73c519500573

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+def anagram_checker str, word
+    # Split both strings into characters and sort alphabetically
+    # Use equality operator to return true or false based on match
+    str.chars.sort == word.chars.sort
+end
+
+class Anagram
+
+    def initialize word
+        @word = word
+    end
+
+    def match arr
+        arr.filter do |str|
+            str == @word || anagram_checker(str, @word)
+        end
+    end
+end
